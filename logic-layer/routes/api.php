d888921700f652c3be3a6c06cb714238dddd5c81
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2025-05-15 13:59:07 +0200

    the artisan works now, but I still don't have a working connection to the db, althought all configs are correctly inserted

--- logic-layer/routes/api.php
+++ logic-layer/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
-use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\Route;
 
-Route::apiResource('customers', CustomerController::class);
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
+
