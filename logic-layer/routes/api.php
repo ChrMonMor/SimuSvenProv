3939af1e1106d2e929be36304b41faf9b4a455db
ChrMonMor <97163196+ChrMonMor@users.noreply.github.com>
2025-05-20 14:17:27 +0200

    got it

--- logic-layer/routes/api.php
+++ logic-layer/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\TypeController;
-use App\Http\Controllers\TagController;
 use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SubcategoryController;
 use App\Http\Controllers\CategorySubcategoryController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\SubcategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TypeController;
 
 Route::apiResource('customers', CustomerController::class);
 Route::apiResource('categories', CategoryController::class);
@@ -15,7 +15,7 @@ Route::apiResource('subcategories', SubcategoryController::class);
 Route::apiResource('categories-subcategories', CategorySubcategoryController::class);
 Route::apiResource('types', TypeController::class);
 Route::apiResource('tags', TagController::class);
-Route::apiResource('item', ItemController::class);
+Route::apiResource('items', ItemController::class);
 
 // testing
 Route::get('/ping', function () {
